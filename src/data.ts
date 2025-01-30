9030e6c3e2e643253dabe71872180e0205b6cc35
OTheNonE <olavnon@gmail.com>
2025-01-30 18:08:12 +0100

    first commit

---
+++ src/data.ts
@@ -0,0 +1,58 @@
+
+export type Person = {
+    id: number,
+    name: string,
+    age: number,
+    city_id: number,
+}
+
+export type City = {
+    id: number,
+    location_name: string,
+    continent: string,
+}
+
+export const persons: Array<Person> = [
+    {
+        id: 1,
+        name: "Charles",
+        age: 5,
+        city_id: 1,
+    },
+    {
+        id: 2,
+        name: "Henry",
+        age: 10,
+        city_id: 2,
+    },
+    {
+        id: 3,
+        name: "Peter",
+        age: 15,
+        city_id: 3,
+    },
+    {
+        id: 4,
+        name: "Lucas",
+        age: 20,
+        city_id: 4,
+    },
+]
+
+export const cities: Array<City> = [
+    {
+        id: 1,
+        location_name: "Copenhagen",
+        continent: "Europe",
+    },
+    {
+        id: 2,
+        location_name: "New York",
+        continent: "America"
+    },
+    {
+        id: 3,
+        location_name: "India",
+        continent: "Asia"
+    }
+]
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+
+import { type City, type Person, persons, cities } from "./data.ts"
+
+
+function LEFT_JOIN<L extends Object, R extends Object>(
+    table_left: Array<L>,
+    table_right: Array<R>,
+    condition: (row_left: L, row_right: R) => boolean
+): Array<L & Partial<R>> {
+
+    const joined_table = table_left.map(row_left => {
+
+        const found_row_right: Partial<R> = table_right.find(row_right => condition(row_left, row_right)) ?? {}
+
+        return { ...found_row_right, ...row_left }
+    })
+
+    return joined_table
+}
+
+function RIGHT_JOIN<
+    L extends Object, R extends Object
+>(
+    table_left: Array<L>, table_right: Array<R>,
+    condition: (row_left: L, row_right: R) => boolean
+): Array<L & Partial<R>> {
+
+    const joined_table = table_left.map(row_left => {
+
+        const found_row_right: Partial<R> = table_right.find(row_right => condition(row_left, row_right)) ?? {}
+
+        return { ...found_row_right, ...row_left }
+    })
+
+    return joined_table
+}
+
+
+
+const left_joined_table = LEFT_JOIN(persons, cities, (person, city) => person.city_id == city.id)
+
+console.log({ left_joined_table })
+left_joined_table[0].location_name
+
